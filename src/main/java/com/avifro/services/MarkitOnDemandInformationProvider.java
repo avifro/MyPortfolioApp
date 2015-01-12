b31e6860c0d3f3547faf3f5e29d4b76846a00669
avifro <avifro@gmail.com>
2015-01-13 01:48:37 +0200

    fix MarkitOnDemandInformationProvider

--- src/main/java/com/avifro/services/MarkitOnDemandInformationProvider.java
+++ src/main/java/com/avifro/services/MarkitOnDemandInformationProvider.java
@@ -3,8 +3,7 @@ package com.avifro.services;
 import com.avifro.entities.UpdatesMessage;
 import com.avifro.http.AvifroJerseyClient;
 
-import javax.xml.ws.Response;
-import java.util.ArrayList;
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,11 +18,65 @@ public class MarkitOnDemandInformationProvider implements StockExchangeInformati
 
     @Override
     public List<UpdatesMessage> checkForUpdatesBySymbols(List<String> symbols) {
-        List<UpdatesMessage> updatesMessages = new ArrayList<>(symbols.size());
-        List<Response> responses = symbols.parallelStream()
-                                          .map(symbol -> jerseyClient.get("/Quote?" + symbol, null))
-                                          .collect(Collectors.toList());
+        return symbols.parallelStream()
+                      .map(symbol -> jerseyClient.get("/Quote?" + symbol, null))
+                      .map(response -> createUpdatesMessage(response))
+                      .collect(Collectors.toList());
+    }
 
-        return updatesMessages;
+    private UpdatesMessage createUpdatesMessage(Response response) {
+        UpdatesMessage updatesMessage = null;
+        StockQuote stockQuote = response.readEntity(StockQuote.class);
+        
+        return updatesMessage;
+    }
+
+
+//    <StockQuote>
+//        <Status>SUCCESS</Status>
+//        <Name>Hewlett-Packard Co</Name>
+//        <Symbol>HPQ</Symbol>
+//        <LastPrice>39.93</LastPrice>
+//        <Change>-0.74</Change>
+//        <ChangePercent>-1.8195229899</ChangePercent>
+//        <Timestamp>Mon Jan 12 15:59:00 UTC-05:00 2015</Timestamp>
+//        <MSDate>42016.6659722222</MSDate>
+//        <MarketCap>73234894260</MarketCap>
+//        <Volume>1851706</Volume>
+//        <ChangeYTD>40.13</ChangeYTD>
+//        <ChangePercentYTD>-0.4983802641</ChangePercentYTD>
+//        <High>40.78</High>
+//        <Low>39.77</Low>
+//        <Open>40.67</Open>
+//    </StockQuote>
+
+    private class StockQuote {
+        private String symbol;
+        private double change;
+        private double lastPrice;
+
+        public String getSymbol() {
+            return symbol;
+        }
+
+        public void setSymbol(String symbol) {
+            this.symbol = symbol;
+        }
+
+        public double getLastPrice() {
+            return lastPrice;
+        }
+
+        public void setLastPrice(double lastPrice) {
+            this.lastPrice = lastPrice;
+        }
+
+        public double getChange() {
+            return change;
+        }
+
+        public void setChange(double change) {
+            this.change = change;
+        }
     }
 }
